787c926b407320a21c64c76874266e6ce84d9056
lastovicka <lastovicka@avast.com>
2016-10-20 22:04:30 +0200

    Extjs project removal

--- client/app/view/main/RunTableController.js
+++
@@ -1,8 +0,0 @@
-Ext.define("MyHealth.view.main.RunTableController", {
-    extend: "Ext.app.ViewController",
-    alias: "controller.run",
-    
-    onAddRun: function() {
-        window.alert("Je to tam.....");
-    }
-});
